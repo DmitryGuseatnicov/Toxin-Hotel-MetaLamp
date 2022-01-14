cbce5f71195ce49e6a4ba68ecffad283da71069b
Dmitry Guseatnicov <dimagusi111@gmail.com>
2022-01-15 01:41:53 +0300

    Сверстал форму логина

---
+++ src/components/login-form/index.js
@@ -0,0 +1,5 @@
+import './../widget-title'
+import './../input'
+import './../button'
+
+import './login-form.scss'
--- src/pages/ui-kit-cards/ui-kit-cards.js
+++ src/pages/ui-kit-cards/ui-kit-cards.js
@@ -3,5 +3,6 @@ import 'material-icons'
 import './../../components/room-search-form'
 import './../../components/register-account-form'
 import './../../components/booking-form'
+import './../../components/login-form'
 
 import './ui-kit-cards.scss'
