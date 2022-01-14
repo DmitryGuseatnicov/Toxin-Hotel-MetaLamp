39922d59f5c3563b3c3fd1572cacabe79792f884
Dmitry Guseatnicov <dimagusi111@gmail.com>
2022-01-15 01:20:20 +0300

    Сверстал форму бронирования номера

---
+++ src/components/booking-form/index.js
@@ -0,0 +1,4 @@
+import './../date-dropdown'
+import './../dropdown'
+import './../button'
+import './booking-form.scss'
--- src/pages/ui-kit-cards/ui-kit-cards.js
+++ src/pages/ui-kit-cards/ui-kit-cards.js
@@ -1,5 +1,7 @@
 import 'material-icons'
 
 import './../../components/room-search-form'
-import '../../components/register-account-form'
+import './../../components/register-account-form'
+import './../../components/booking-form'
+
 import './ui-kit-cards.scss'
