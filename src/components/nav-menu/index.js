b62684a422e178ad2244e39d03c2e28754fdd5ac
Dmitry Guseatnicov <dimagusi111@gmail.com>
2022-01-15 19:53:03 +0300

    Сверстал NavMenu Logo и Header, добавил логику для NavMenu

---
+++ src/components/nav-menu/index.js
@@ -0,0 +1,5 @@
+import $ from 'jquery'
+import { NavMenu } from './NavMenu'
+import './nav-menu.scss'
+
+new NavMenu($('.nav'))
---
+++ src/pages/ui-kit-header-footer/ui-kit-header-footer.js
@@ -0,0 +1,3 @@
+import 'material-icons'
+import './../../components/header'
+import './ui-kit-header-footer.scss'
