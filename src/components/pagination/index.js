7a9ce0070ec3bba3096608624d8eb6dfc2a090f7
Dmitry Guseatnicov <dimagusi111@gmail.com>
2022-03-25 02:16:58 +0300

    Избавился от не нужных вложеных селекторах, добавил цеклы для footer-social

--- src/components/pagination/index.js
+++ src/components/pagination/index.js
@@ -3,19 +3,21 @@ import $ from 'jquery';
 import Pagination from './Classes/Pagination';
 import './pagination.scss';
 
-const pagination = $('.js-pagination').map((i, el) => new Pagination(el));
-pagination[0].update({ currentPage: 1, totalItems: 180 });
+const $pagination = $('.js-pagination');
+
+const [pagination] = $pagination.map((i, el) => new Pagination(el));
+pagination.update({ currentPage: 1, totalItems: 180 });
 
 function changePage(e) {
   if (e.target.closest('.pagination__action-next')) {
-    pagination[0].update({ currentPage: pagination[0].currentPage + 1 });
+    pagination.update({ currentPage: pagination.currentPage + 1 });
   }
   if (e.target.closest('.pagination__action-prev')) {
-    pagination[0].update({ currentPage: pagination[0].currentPage - 1 });
+    pagination.update({ currentPage: pagination.currentPage - 1 });
   }
   if (e.target.closest('.pagination__page-number')) {
-    pagination[0].update({ currentPage: +e.target.textContent });
+    pagination.update({ currentPage: +e.target.textContent });
   }
 }
 
-pagination.on('click', changePage);
+$pagination.on('click', changePage);
