44a19efc73e71bc526f0e0a5beac9c23bb4d3a02
Dmitry Guseatnicov <dimagusi111@gmail.com>
2022-04-06 15:36:04 +0300

    Добавил разделения в значениях в range-slider #3

--- src/components/range-slider/Classes/RangeSlider.js
+++ src/components/range-slider/Classes/RangeSlider.js
@@ -1,8 +1,13 @@
 import $ from 'jquery';
 import noUiSlider from 'nouislider';
+import wNumb from 'wnumb';
 
 import constants from '../utils/constants';
 import 'nouislider/dist/nouislider.css';
+
+const numFormate = wNumb({
+  thousand: ' ',
+});
 
 class rangeSlider {
   constructor(nodeElem) {
@@ -35,7 +40,8 @@ class rangeSlider {
   changeHandler(values) {
     this.$start.val(values[0]);
     this.$end.val(values[1]);
-    this.$value.text(`${Math.round(values[0])}₽ - ${Math.round(values[1])}₽`);
+
+    this.$value.text(`${numFormate.to(+values[0])}₽ - ${numFormate.to(+values[1])}₽`);
   }
 }
 
