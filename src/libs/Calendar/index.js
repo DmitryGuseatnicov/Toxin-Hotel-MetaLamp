397cf4ff4a7ae77e5325a96bd32d68d52e7097ce
DmitryGuseatnicov <dimagusi111@gmail.com>
2022-06-09 19:37:48 +0300

    Fixed colored interval in calendar of selected dates 6.3 #6

--- src/libs/Calendar/index.js
+++ src/libs/Calendar/index.js
@@ -11,6 +11,25 @@ class Calendar {
 
   init(nodeElem) {
     this._createCalender(nodeElem);
+    this._bindEventListener();
+  }
+
+  _bindEventListener() {
+    this._clickHandler = this._clickHandler.bind(this);
+    this.calendar.$datepicker.addEventListener('click', this._clickHandler);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  _clickHandler(e) {
+    const isClickedOnlyFrom =
+      e.target.closest(
+        '.air-datepicker-cell',
+        '.-day-',
+        '.-selected-',
+        '.-range-from-'
+      ) && this.calendar.selectedDates.length <= 1;
+
+    if (isClickedOnlyFrom) e.target.classList.add('-range-to-');
   }
 
   _createCalender(nodeElem) {
